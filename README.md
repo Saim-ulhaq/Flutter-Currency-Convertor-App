6ab5ecec03a5955cb5c40ab31b5ccbcf7fdd4444
M.Saim-Ulhaq <114234931+Saim-ulhaq@users.noreply.github.com>
2024-01-22 14:14:00 +0500

    Initial
    Initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Flutter Currency Convertor App
  This is the Basic Currency Convertor App in Flutter 
+ Created By M.Saim-Ul-Haq
+ 
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:currency_convertor/currency_converter_material_page.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// Types of Widgets
+// 1. StatelessWidget
+// 2. StatefulWidget
+
+// State
+
+// 1. Material Design
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConverterMaterialPage(),
+    );
+  }
+}
+
+
